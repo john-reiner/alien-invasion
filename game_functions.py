f98292c68d35ec35734dcc5d2b74db1928d93e16
john-reiner <johnreiner1990@gmail.com>
2020-10-06 17:58:48 -0400

    the fleet appears on screen

--- game_functions.py
+++ game_functions.py
@@ -71,13 +71,15 @@ def create_fleet(ai_settings, screen, aliens):
     # Spacing between each alien is equal to one alien width. 
     alien = Alien(ai_settings, screen)
     alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width -2 * alien_width 
-    number_aliens_x = int(available_space_x/ (2 * alien_width))
+    available_space_x = ai_settings.screen_width - 2 * alien_width 
+    number_aliens_x = int(available_space_x / (2 * alien_width))
 
     # Create the first row of aliens.
     for alien_number in range(number_aliens_x):
+        # Create an alien and place it in the row. 
         alien = Alien(ai_settings, screen)
-        alien.x = alien_width + 2 * alien_width * alien_number 
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x 
         aliens.add(alien)
-        
+
 
