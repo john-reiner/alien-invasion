45acd0a82190e8e89f7e4274bb380d2eb400007f
john-reiner <johnreiner1990@gmail.com>
2020-10-06 17:54:18 -0400

    seting up the fleet of aliens

--- game_functions.py
+++ game_functions.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 from bullet import Bullet 
+from alien import Alien
 
 def update_bullets(bullets):
     """Upadate posion of bullets and get rid of old bullets."""
@@ -50,7 +51,7 @@ def check_events(ai_settings, screen, ship, bullets):
             check_keyup_events(event, ship)
 
 
-def update_screen(ai_settings, screen, ship, alien, bullets):
+def update_screen(ai_settings, screen, ship, aliens, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -59,7 +60,24 @@ def update_screen(ai_settings, screen, ship, alien, bullets):
         bullet.draw_bullet()
         
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
 
     # Make the most recently drawn screen visible. 
     pygame.display.flip()
+
+def create_fleet(ai_settings, screen, aliens):
+    """Create a full fleet of aliens."""
+    # Create an alien and find the number of aliens in a row. 
+    # Spacing between each alien is equal to one alien width. 
+    alien = Alien(ai_settings, screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width -2 * alien_width 
+    number_aliens_x = int(available_space_x/ (2 * alien_width))
+
+    # Create the first row of aliens.
+    for alien_number in range(number_aliens_x):
+        alien = Alien(ai_settings, screen)
+        alien.x = alien_width + 2 * alien_width * alien_number 
+        aliens.add(alien)
+        
+
