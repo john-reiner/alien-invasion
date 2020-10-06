7bbd877eaddf38730191a16227f9b31d34eb3e92
john-reiner <johnreiner1990@gmail.com>
2020-10-05 21:34:54 -0400

    limited the number of bullets allowed to be fired on the game

--- settings.py
+++ settings.py
@@ -12,9 +12,9 @@ class Settings():
         self.ship_speed_factor = 3.5
 
         # Bullet settings 
-        self.bullet_speed_factor = 5
+        self.bullet_speed_factor = 15
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
-        
+        self.bullets_allowed = 3 
 
