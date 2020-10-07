a1955b812a5fdec014cf12f8284b7945f3985bd6
john-reiner <johnreiner1990@gmail.com>
2020-10-07 07:50:59 -0400

    set up ship_hit function to reset game

--- game_functions.py
+++ game_functions.py
@@ -1,5 +1,7 @@
 import sys
+from time import sleep
 import pygame
+
 
 from bullet import Bullet 
 from alien import Alien
@@ -123,14 +125,30 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed 
     ai_settings.fleet_direction *= -1 
 
-def update_aliens(ai_settings, ship, aliens):
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+    """Respond to ship being hit by alien."""
+    # Decrement ships_left. 
+    stats.ships_left -= 1
+
+    # empty the list of aliens and bullets. 
+    aliens.empty()
+    bullets.empty()
+
+    # Create a new fleet and center the ship.
+    create_fleet(ai_settings, screen, ship, aliens)
+    ship.center_ship()
+
+    # Pause. 
+    sleep(0.5)
+
+def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """Check if the fleet is at an edge, and then update the positions of all aliens in the fleet."""
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
 
     # Look for alien-ship collisions. 
     if pygame.sprite.spritecollideany(ship, aliens):
-        print("SHIP HIT!!")
+        ship_hit(ai_settings, stats, screen, ship, aliens, bullets) 
 
 
 
