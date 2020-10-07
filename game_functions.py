0dced1296e2c75e1e5a6d96b0cb0f92eb587402e
john-reiner <johnreiner1990@gmail.com>
2020-10-07 07:55:42 -0400

    updated game_functions to check if aliens hit the bottom of the screen

--- game_functions.py
+++ game_functions.py
@@ -141,6 +141,15 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     # Pause. 
     sleep(0.5)
 
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Check if any aliens have reached the bottom of the screen."""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites(): 
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Treat this the same as if the ship got hit 
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break 
+
 def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """Check if the fleet is at an edge, and then update the positions of all aliens in the fleet."""
     check_fleet_edges(ai_settings, aliens)
@@ -149,6 +158,10 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     # Look for alien-ship collisions. 
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets) 
+
+    # Look for aliens hitting the bottom of the screen. 
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+
 
 
 
