8bd3e6233ae64adecb6869d9f1ee76634e4b2ea1
john-reiner <johnreiner1990@gmail.com>
2020-10-07 07:43:48 -0400

    added GameStats to alien_invasion file

--- settings.py
+++ settings.py
@@ -8,8 +8,9 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230,230,230)
 
-        # Ship speed settings
+        # Ship settings
         self.ship_speed_factor = 5
+        self.ship_limit = 3
 
         # Bullet settings 
         self.bullet_speed_factor = 15
