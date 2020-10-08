0384bcc9b16939bc4a602cd3cc972b5d605d198d
john-reiner <johnreiner1990@gmail.com>
2020-10-08 19:27:11 -0400

    setting up scoreboard

---
+++ scoreboard.py
@@ -0,0 +1,10 @@
+import pygame.font
+
+class Scoreboard():
+
+    """A class to report scoring information."""
+
+    def __init__(self, ai_settings, screen, stats):
+        """ Initialize scorekeeping attributes."""
+        sel.screen = screen
+        
