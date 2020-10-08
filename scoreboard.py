536b131c691c78a40b250a3da95b86398f3db3e1
john-reiner <johnreiner1990@gmail.com>
2020-10-08 19:29:33 -0400

    set up scoreboard class

--- scoreboard.py
+++ scoreboard.py
@@ -7,4 +7,14 @@ class Scoreboard():
     def __init__(self, ai_settings, screen, stats):
         """ Initialize scorekeeping attributes."""
         sel.screen = screen
+        self.screen_rect = screen.get_rect()
+        self.ai_settings = ai_settings
+        self.stats = stats
+
+        # Font settings for scoring information. 
+        self.text_color = (30,30,30)
+        self.font = pygame.font.SysFont(None, 48)
+
+        # Prepare the initial score image. 
+        self.prep_score()
         
