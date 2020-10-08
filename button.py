626d97e68af57e9b3634d821163954cd91af481c
john-reiner <johnreiner1990@gmail.com>
2020-10-08 11:53:40 -0400

    play button works

--- button.py
+++ button.py
@@ -1,12 +1,13 @@
 import pygame.font 
 
 class Button():
+
     def __init__(self, ai_settings, screen, msg):
         """Initialize button attributes."""
         self.screen = screen 
         self.screen_rect = screen.get_rect()
 
-        # Set the dimensions and properties of the button. 
+        # Set the dimensions and properties of the button.
         self.width, self.height = 200, 50
         self.button_color = (0, 255, 0)
         self.text_color = (255, 255, 255)
@@ -27,5 +28,6 @@ class Button():
 
     def draw_button(self):
         # Draw blank button and then draw message. 
+        
         self.screen.fill(self.button_color, self.rect)
         self.screen.blit(self.msg_image, self.msg_image_rect)
